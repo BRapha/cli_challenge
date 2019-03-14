f4dcb688d4acab1ae0e7841f3ce500241adbdc3d
Raphael Blistein <raphael.blistein@gmail.com>
2019-03-14 16:55:52 +0100

    final commit
    tried implementing ‚GET‘ and ‚POST‘ requests

--- tabs_service/__init__.py
+++ tabs_service/__init__.py
@@ -1,5 +1,9 @@
 from flask import Flask
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'service_db'
+app.config["MONGO_URI"] = "mongodb://localhost:27017/service_db"
+mongo = PyMongo(app)
 
 from tabs_service import routes
--- tabs_service/routes.py
+++ tabs_service/routes.py
@@ -1,27 +1,43 @@
-from tabs_service import app
+from tabs_service import app, mongo
 from flask import request, jsonify
+from bson.json_util import loads, dumps
+
 
 @app.route('/')
 def home():
-    return "Hello World"
+    return "Use /tabs route to access tabs."
 
 
-@app.route('/tabs', methods=['GET', 'POST'])
-def tabs():
-    if request.method == 'GET':
-        print("GET tabs")
-    elif request.method == 'POST':
-        content = request.get_json()
-        print(content)
-        print("POST tabs")
-    return 'Done'
+@app.route('/tabs', methods=['GET'])
+def get_all_tabs():
+    # returns JSON array of all tabs in DB
+    out = []
+    for tab in mongo.db.tabs.find():
+        out.append(tab)
+
+    return jsonify(out)
 
 
-@app.route('/tabs/<int:tabs_id>', methods=['PUT', 'DELETE'])
-def change_tabs(tabs_id):
-    if request.method == 'DELETE':
-        print(f"DELETE tabs with id: {tabs_id}")
-    elif request.method == 'PUT':
-        print(f"UPDATE tabs with id: {tabs_id}")
+@app.route('/tabs', methods=['POST'])
+def save_tab():
+    content = request.get_json()
+    print(f"response = {content}")
+    if not content:
+        return "Please provide a JSON object in the request body!"
 
-    return 'Done'
+    tab_collection = mongo.db.tabs
+    tab_id = tab_collection.insert(content)
+    return "New tabs inserted"
+
+
+@app.route('/tabs/<int:tab_id>', methods=['PUT'])
+def override_tab(tab_id):
+    #TODO: Override tab from MongoDB
+    return f"Did override {tabs_id}"
+
+
+
+@app.route('/tabs/<int:tab_id>', methods=['DELETE'])
+def delete_tab(tab_id):
+    #TODO: Delete tab from MongoDB
+    return f"Deleted {tab_id}"
